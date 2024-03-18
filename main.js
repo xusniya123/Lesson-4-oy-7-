9ca0ee4784bef56b4fb57b34301d41175908e881
xusniya123 <xusniya12345678@gmail.com>
2024-03-18 17:18:03 +0500

    Lesson 4 moth (7)

---
+++ main.js
@@ -0,0 +1,32 @@
+// const front951 = ["Madinaxon", "Nasiba", "Xusniya" ];
+// let name1 = front951[1];
+
+// alert(name1);
+
+// const front952 = [];
+// front952[0] = "Madinaxon";
+// front952[1] = "Nasiba";
+// front952[2] = "Xusniya";
+
+// alert(front952);
+
+// let front953 = ["Madinaxon", "Nasiba", "xusniya", "Jasur", ]
+
+
+
+let fruits = ["olma", "Banan", "Gilos", "Shoftoli"];
+console.log(fruits);
+let long = fruits.length ;
+alert("Boshlanishidagi Arrayning uzunligi: " + long);
+let result = confirm ("Sizning arrayingizning ma'lumotlarini olib tashlamoqchimisiz");
+if(result) {
+    fruits.pop(1);
+    let uzunlik = fruits. length;
+    alert("Array uzunligi:" + uzunlik);
+    console.log (fruits);
+    alert.log (fruits)
+}else{
+    let uzunlik = fruits. length;
+    console.log(fruits);
+    alert.log (fruits);
+}
